2f3f7683fa08fe4114441d0544b531d2f90e7302
Tim Zimmermann <tim@linux4.de>
2024-05-09 06:48:24 +0200

    a71-common: Use pixel power HAL directly
    * Samsung one is outdated and we didn't do any meaningful changes there
    
    Change-Id: I65378563396d12ee76e7f2b34105988c7e66cf38

--- common.mk
+++ common.mk
@@ -393,7 +393,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.samsung-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.3.vendor \
 
@@ -531,7 +531,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/samsung \
-    hardware/samsung/aidl/power-libperfmgr \
     vendor/qcom/opensource/usb/etc \
 
 # Telephony
