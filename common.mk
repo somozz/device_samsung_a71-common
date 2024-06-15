5a85a9fe5b6d505b4f60726e18417cd98807cc6b
Tim Zimmermann <tim@linux4.de>
2024-06-15 18:10:57 +0200

    a71-common: light: Convert to AIDL implementation
    * Built from hardware/samsung.
    
    Change-Id: Ie15a6316a12abd00b83c6db988692f581660711f

--- common.mk
+++ common.mk
@@ -249,9 +249,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.sm6150 \
+    android.hardware.light-service.samsung \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
