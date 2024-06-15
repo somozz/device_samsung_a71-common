13a09202c42ddf0e402ec6631701b466a6e1ff98
Patrick Lower <devvortex@gmail.com>
2024-06-15 12:38:06 +0200

    a71-common: Build the `SamsungDoze` from source
    * Built from hardware/samsung.
    
    Change-Id: I80ba7d4ac5478a1cb93b994d66972ac392981a5c

--- common.mk
+++ common.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDAP \
 
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze \
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
