e0ef8c6dae80cdf5667a6cb0bef47518a8344755
xSylla <syllamodder@gmail.com>
2024-06-15 17:58:30 +0200

    a71-common: Enforce fstab suffix to qcom
    Change-Id: I0d203dc3f3390f3805cfb5c549d84a1f245db7dc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,7 +144,7 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_DTB_OFFSET := 0x01F00000
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_OFFSET := 0x00008000
