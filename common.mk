693aaa2e9bed52f1dafc1f2629f41f09725ecc71
Haky86 <hakamelassouad@gmail.com>
2024-06-15 16:46:31 +0200

    a71-common: Drop all includes non-existent modules in PRODUCT_PACKAGES
    * Fixes:
    Offending entries:
    android.hardware.broadcastradio@1.0-service
    android.hardware.camera.provider@2.5-impl
    android.hardware.gnss@2.1-service-qti
    android.hardware.graphics.composer@2.4-impl
    android.hardware.neuralnetworks@1.3-service-qti
    android.hardware.usb.gadget@1.0-service-qti
    com.qualcomm.qti.bluetooth_audio@1.0
    com.qualcomm.qti.bluetooth_audio@1.0.vendor
    libaudioconfigstore
    libbluetooth_qti
    libdisplayconfig.qti.vendor
    libqdMetaData.vendor
    libskeymaster4device.vendor
    textclassifier.bundle1
    vendor.display.color@1.5.vendor
    vendor.display.postproc@1.0.vendor
    
    Change-Id: Ief3ed135debf8957dfb21d18f914334b929d4176

--- common.mk
+++ common.mk
@@ -54,7 +54,6 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     cplay \
-    libaudioconfigstore \
     libaudiofoundation \
     libaudioroute \
     libaudioutils \
@@ -129,10 +128,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     aptxalsOverlay \
     audio.bluetooth.default \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbluetooth_audio_session \
-    libbluetooth_qti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
@@ -143,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.device@2.0.vendor \
     android.frameworks.cameraservice.service@2.1.vendor \
     android.hardware.camera.device@3.7.vendor \
-    android.hardware.camera.provider@2.5-impl \
     android.hardware.camera.provider@2.6-service.sm6150 \
     android.hardware.camera.provider@2.7.vendor \
     libcamera2ndk_vendor \
@@ -203,7 +198,6 @@ PRODUCT_PACKAGES += \
 # FM
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.broadcastradio@1.0-service \
 
 PRODUCT_PACKAGES += \
     FM2 \
@@ -218,7 +212,6 @@ PRODUCT_PACKAGES += \
 
 # GNSS/GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1-service-qti \
     android.hardware.gnss@2.1.vendor \
 
 PRODUCT_COPY_FILES += \
@@ -233,25 +226,21 @@ PRODUCT_COPY_FILES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-impl \
+    android.hardware.graphics.composer@2.4-hal \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     gralloc.sm6150 \
     hwcomposer.sm6150 \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libqdMetaData \
-    libqdMetaData.vendor \
     libsdmcore \
     libsdmutils \
     libtinyxml \
     libion \
     libvulkan \
-    vendor.display.color@1.5.vendor \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0.vendor \
-    vendor.display.postproc@1.0.vendor \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@1.0.vendor \
@@ -288,7 +277,6 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \
-    libskeymaster4device.vendor:64 \
     libkeymaster4support.vendor:64 \
     libkeymaster4_1support.vendor:64 \
 
@@ -342,10 +330,6 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     android.system.net.netd@1.1 \
     netutils-wrapper-1.0 \
-
-# Neural Networks
-PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.3-service-qti \
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -589,10 +573,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext \
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1 \
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti \
@@ -607,7 +587,6 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb.gadget@1.0-service-qti \
     android.hardware.usb@1.1.vendor \
 
 # Vendor Service Manager
