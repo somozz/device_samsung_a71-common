387a0872a888bd8c10766968f49e0df024f92f45
Bruno Martins <bgcngm@gmail.com>
2024-06-15 09:49:24 +0200

    a71-common: Setup default bluetooth config header
    * Straight from CAF (as per LA.UM.6.3.r4-04300-sdm845.0), with BTM_DEF_LOCAL_NAME unset so that ro.product.model is used instead.
    
    Change-Id: If6ba5c8a19f73a7a7c3a86fceecb1c9c075f913f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,5 +45,8 @@ BOARD_USES_ALSA_AUDIO := true
 SOUND_TRIGGER_FEATURE_LPMA_ENABLED := true
 USE_XML_AUDIO_POLICY_CONF := 1
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/include
+
 # Get non-open-source specific aspects
 include vendor/samsung/a71-common/BoardConfigVendor.mk
