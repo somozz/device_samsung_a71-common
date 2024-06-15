152ace467b7a0bc69c79bc9c42b6ba4a7a12c0f1
Patrick Tjin <pattjin@google.com>
2024-06-15 11:33:49 +0200

    a71-common: Add no SDCard to AAPT characteristics
    Change-Id: I632523f726107f30742b9e7f4f0711456fdb007d

--- common.mk
+++ common.mk
@@ -16,6 +16,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 COMMON_PATH := device/samsung/a71-common
 
 PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_TARGET_VNDK_VERSION := 30
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
