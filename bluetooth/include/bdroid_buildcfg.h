867f31e4de0573ecacb0c0cae5aed921be512dce
Sal Savage <salsavage@google.com>
2024-06-15 16:36:40 +0200

    a71-common: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: If8a95f928cbdab3732abb75618d2797f1ee9be62

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -7,8 +7,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy A71"
-
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
