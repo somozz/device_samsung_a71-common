ddb26f30cd9ce1fb9cec2fa51867cca28e1faf2a
Haky86 <hakamelassouad@gmail.com>
2024-06-15 14:57:05 +0200

    a71-common: Update WiFi configs from sunfish
    * Stock one have some issues with AOSP.
    * Fixes random disconnection.
    
    Change-Id: I4554a2cc8ce1908d037e9d7820bfcb6e38836169

--- common.mk
+++ common.mk
@@ -679,7 +679,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/vendor/etc/wifi/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
     $(COMMON_PATH)/rootdir/vendor/etc/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
     $(COMMON_PATH)/rootdir/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/rootdir/vendor/etc/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
