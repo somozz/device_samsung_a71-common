d0f5c1997aa5139780aa44dab51f2c9867867cba
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-06-15 18:09:46 +0200

    a71-common: Build fastcharge implementation from source
    * Built from hardware/samsung.
    
    Change-Id: I9327f064af1817053425523dfddec5161b30b2b7

--- common.mk
+++ common.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images_vendor \
     libsuspend \
-    vendor.lineage.fastcharge@1.0-service.a71 \
+    vendor.lineage.fastcharge@1.0-service.samsung \
 
 # Data
 $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_product.mk)
