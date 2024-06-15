1e6538f54be59eab3f5c5caeeb4e4320731049d1
Haky86 <hakamelassouad@gmail.com>
2024-06-15 11:20:43 +0200

    a71-common: Import AID/fs from CAF
    * Taken from https://git.codelinaro.org/clo/la/device/qcom/common/-/blob/LA.UM.9.1.c11-56900-SMxxx0.QSSI13.0.
    
    Change-Id: I12f2eccfeefcd336cd607fff64fec0808e20d374

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -210,5 +210,8 @@ TARGET_USES_MKE2FS := true
 BOARD_PROVIDES_LIBRIL := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Rootfs
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+
 # Get non-open-source specific aspects
 include vendor/samsung/a71-common/BoardConfigVendor.mk
