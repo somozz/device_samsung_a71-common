6f6267d4072b39e0228eec070c22321fe1243022
Haky86 <hakamelassouad@gmail.com>
2024-06-15 09:55:00 +0200

    a71-common: Setup `BOARD_USES_DPM` flag
    Change-Id: I26e1338868f32dc6c764de9db30b2adc3c84bb96

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,5 +72,8 @@ BOARD_USES_QCNE := true
 # Dexpreopt
 BOARD_USES_SYSTEM_OTHER_ODEX := true
 
+# DPM
+BOARD_USES_DPM := true
+
 # Get non-open-source specific aspects
 include vendor/samsung/a71-common/BoardConfigVendor.mk
