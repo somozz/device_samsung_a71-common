bc60158f4d7167d50b280c27a7a47fa6caca7d0e
danascape <danascape@gmail.com>
2024-07-31 14:12:47 +0200

    a71-common: Drop atrace HAL
    It is deprecated in Android 14.
    
    Change-Id: Ifaf4a60612a37d193f2d116b486f19ad3c432844

--- common.mk
+++ common.mk
@@ -34,10 +34,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service \
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl.sm6150 \
